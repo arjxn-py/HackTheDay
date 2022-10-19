c6b95bba4278377ac6552fe72f9f2473ac74f4aa
itAmircic <arjxn.py@gmail.com>
2022-10-19 23:47:41 +0530

    Display filter options  and show the selected one

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import TodoList from './TodoList';
-
 class App extends Component {
     constructor() {
         super();
         this.state = {
+            filter: 'active',
             items: [
                 {
                     id: 1,
@@ -24,22 +24,19 @@ class App extends Component {
             ]
         }
     }
-
     render() {
         let title = 'Things to do';
-
         return (
             <div className="container">
                 <div className="row">
                     <TodoList title={title}
                         addNew={this.addNew.bind(this)}
-                        items={this.state.items}
+                        {...this.state}
                     />
                 </div>
             </div>
         );
     }
-
     addNew(text) {
         let nextId = this.state.items.length + 1
         let item = {
@@ -47,11 +44,9 @@ class App extends Component {
             text: text
         };
         let updatedList = this.state.items.concat([item]);
-
         this.setState({
             items: updatedList
         })
     }
 }
-
 export default App;
