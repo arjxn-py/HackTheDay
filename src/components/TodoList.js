4881c90b5abfdf08fadcd6a5747bc4f6243dedf5
arjxn.py <arjxn.py@gmail.com>
2022-10-27 18:13:29 +0530

    Move the applyFilter logic to filter service

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,35 +2,21 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import TodoItem from './TodoItem';
+import {applyFilter} from './../services/filter';
+
 function TodoList(props) {
     const {title, items, addNew, filter, changeFilter} = props;
     const count = items.length;
-
     const filteredList = applyFilter(items, filter);
-
     return (
         <div className="todolist">
             <Header title={title} addNew={addNew}/>
-
             <ul className="list-unstyled">
                 {filteredList.map(item => <TodoItem key={item.id} data={item}/>)}
             </ul>
             <Footer {...{count, filter, changeFilter}}/>
         </div>
     );
-}
-
-function applyFilter(list, filter) {
-    switch (filter) {
-        case 'completed':
-            return list.filter(item => item.completed === true);
-
-        case 'active':
-            return list.filter(item => item.completed !== true);
-
-        default:
-            return list;
-    }
 }
 
 export default TodoList;
---
+++ src/services/filter.js
@@ -0,0 +1,12 @@
+export function applyFilter(list, filter) {
+    switch (filter) {
+        case 'completed':
+            return list.filter(item => item.completed === true);
+
+        case 'active':
+            return list.filter(item => item.completed !== true);
+
+        default:
+            return list;
+    }
+}
